44cb883bea9cda08a3e12267ddbda8d6c2929db8
ni3yyn <ni3yyn@gmail.com>
2025-07-22 14:04:18 +0100

    Added admindesign

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,15 @@
 // components/LandingPage.js
 import React from 'react';
 import '../LandingPage.css'; // style it as needed
-
+import logo from '../assets/logo.png';
 
 
 function LandingPage({ onChooseMatcher, onChooseStore }) {
   return (
     <div className="landing-container">
+      <img src={logo} alt="Logo" className="logo" />
       <h1 className="label">مرحبا بك في متجرنا للزيوت الطبيعية</h1>
-      <p className="lanbel">كيفاش تخير الزيت؟</p>
+      <p className="lanbel">كيفاش حاب تخير الزيت؟</p>
 
       <div className="landing-buttons">
         <button onClick={onChooseMatcher} >
